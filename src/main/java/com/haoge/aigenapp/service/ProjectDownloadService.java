37c51e3973853ea394c740c09883cae17682224a
haoge <jiangyihao@golden-sky.com>
2025-08-17 14:09:38 +0800

    下载生成的文件

---
+++ src/main/java/com/haoge/aigenapp/service/ProjectDownloadService.java
@@ -0,0 +1,7 @@
+package com.haoge.aigenapp.service;
+
+import jakarta.servlet.http.HttpServletResponse;
+
+public interface ProjectDownloadService {
+    void downloadProjectAsZip(String projectPath, String downloadFileName, HttpServletResponse response);
+}
