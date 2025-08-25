5de14abb37a92de40169ae24e9d4e2ed3ad12145
haoge <jiangyihao@golden-sky.com>
2025-08-25 22:24:28 +0800

    feat(cache): 实现应用列表页面缓存功能
    - 添加 Redis缓存配置，支持自定义序列化器和过期时间
    - 在 AppController 中为精选应用列表接口添加缓存注解
    - 新增 CacheKeyUtils 工具类，用于生成缓存键- 调整项目构建逻辑，确保缓存数据一致性

--- src/main/java/com/haoge/aigenapp/common/BaseResponse.java
+++ src/main/java/com/haoge/aigenapp/common/BaseResponse.java
@@ -2,6 +2,7 @@ package com.haoge.aigenapp.common;
 
 import com.haoge.aigenapp.exception.ErrorCode;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
@@ -11,6 +12,7 @@ import java.io.Serializable;
  * @param <T>
  */
 @Data
+@NoArgsConstructor
 public class BaseResponse<T> implements Serializable {
 
     private int code;
